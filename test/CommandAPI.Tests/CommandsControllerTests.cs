5a9a00cf520a32013b2788f23d55e35a84433534
oat-man <oat_man@hotmail.com>
2021-03-31 17:45:59 +0700

    Update Unit testing

--- test/CommandAPI.Tests/CommandsControllerTests.cs
+++ test/CommandAPI.Tests/CommandsControllerTests.cs
@@ -5,14 +5,34 @@ using AutoMapper;
 using CommandAPI.Models;
 using CommandAPI.Data;
 using CommandAPI.Profiles;
+using CommandAPI.Dtos;
 using Xunit;
 using CommandAPI.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CommandAPI.Tests
 {
-    public class CommandsControllerTests
+    public class CommandsControllerTests : IDisposable
     {
+        Mock<ICommandAPIRepo> mockRepo;
+        CommandsProfile realProfile;
+        MapperConfiguration configuration;
+        IMapper mapper;
+
+        public CommandsControllerTests()
+        {
+            mockRepo = new Mock<ICommandAPIRepo>();
+            realProfile = new CommandsProfile();
+            configuration = new MapperConfiguration(cfg => cfg.AddProfile(realProfile));
+            mapper = new Mapper(configuration);
+        }
+
+        public void Dispose()
+        {
+
+        }
+
+
         [Fact]
         public void GetCommandItems_Returns200OK_WhenDBIsEmpty()
         {
@@ -29,6 +49,72 @@ namespace CommandAPI.Tests
             IMapper mapper = new Mapper(configuration);
 
             var controller = new CommandsController(mockRepo.Object, mapper);
+
+            //Act
+            var result = controller.GetAllCommands();
+
+            //Asset
+            Assert.IsType<OkObjectResult>(result.Result);
+
+        }
+
+        [Fact]
+        public void GetAllCommands_ReturnsOneItem_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+                repo.GetAllCommands()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+
+            //Act
+            var result = controller.GetAllCommands();
+
+            //Assert
+            var okResult = result.Result as OkObjectResult;
+            var commands = okResult.Value as List<CommandReadDto>;
+
+            Assert.Single(commands);
+
+        }
+
+
+        [Fact]
+        public void GetAllCommands_Returns200OK_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+                repo.GetAllCommands()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+
+            //Act
+            var result = controller.GetAllCommands();
+
+            //Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+        }
+
+
+        [Fact]
+        public void GetAllCommands_ReturnsCorrectType_WhenDBHasOneResource()
+        {
+            //Arrange
+            mockRepo.Setup(repo =>
+                repo.GetAllCommands()).Returns(GetCommands(1));
+
+            var controller = new CommandsController(mockRepo.Object, mapper);
+
+
+            //Act
+            var result = controller.GetAllCommands();
+
+
+            //Assert
+            Assert.IsType<ActionResult<IEnumerable<CommandReadDto>>>(result);
 
         }
 
